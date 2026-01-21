3f4b10995a048715ec3693876616c525286bfa24
PookyFan <pookyfan@kancolle.pl>
2026-01-21 22:26:38 +0100

    Forced register storage for performance critical variables and changed PWM signal generation to purely software one

--- src/main.c
+++ src/main.c
@@ -16,61 +16,67 @@
 #define ADC_HIGH_VAL 247L
 
 static volatile struct {
-    uint16_t display_delay;
     uint16_t fan_revolution_pulses;
     uint16_t fan_revolution_count;
-    uint8_t  periph_delay;
     uint8_t  current_character;
     uint8_t  prev_adc_value;
     uint8_t  curr_adc_value;
-    uint8_t  pwm_enabled;
-    uint8_t  update_fan_speed;
-    uint8_t  display_percentage;
-} global;
+} global_ram;
+
+register struct {
+    uint16_t display_delay;          //r10-r11
+    uint8_t  periph_delay;           //r12
+    uint8_t  pwm_enabled        : 1; //r13.0
+    uint8_t  update_fan_speed   : 1; //r13.1
+    uint8_t  display_percentage : 1; //r13.2
+} global_reg asm("r10");
 
 static void disable_pwm_and_set_pin(uint8_t value)
 {
     OCR0B = 0xFF; //Do not allow further compare match B events
     set_pin(FAN_PWM_PORT, value);
-    clear_bit(TCCR0A, COM0A0);
-    global.pwm_enabled = 0;
+    global_reg.pwm_enabled = 0;
 }
 
 ISR(INT0_vect)
 {
-    ++global.fan_revolution_pulses;
+    ++global_ram.fan_revolution_pulses;
 }
 
-ISR(TIM0_COMPA_vect, ISR_NOBLOCK)
+ISR(TIM0_COMPA_vect)
 {
-    if(++global.display_delay == TIMER_FREQ)
+    if(global_reg.pwm_enabled)
+        set_pin_high(FAN_PWM_PORT);
+
+    sei();
+    if(++global_reg.display_delay == TIMER_FREQ)
     {
-        global.display_delay = 0;
-        global.fan_revolution_count = global.fan_revolution_pulses >> 1;
-        global.fan_revolution_pulses = global.fan_revolution_pulses & 1;
-        global.update_fan_speed = 1;
-        global.display_percentage = 0;
+        global_reg.display_delay = 0;
+        global_ram.fan_revolution_count = global_ram.fan_revolution_pulses >> 1;
+        global_ram.fan_revolution_pulses = global_ram.fan_revolution_pulses & 1;
+        global_reg.update_fan_speed = 1;
+        global_reg.display_percentage = 0;
     }
 
-    if(++global.periph_delay == 104) //For 25 kHz main timer we'll get ~240 Hz peripheral timer
+    if(++global_reg.periph_delay == 104) //For 25 kHz main timer we'll get ~240 Hz peripheral timer
     {
-        global.current_character = (global.current_character + 1) & 3;
-        display_character(global.current_character);
-        global.periph_delay = 0;
+        global_ram.current_character = (global_ram.current_character + 1) & 3;
+        display_character(global_ram.current_character);
+        global_reg.periph_delay = 0;
         set_bit(ADCSRA, ADSC); //Start new ADC conversion while no communication with display is taking place
     }
 }
 
-
 ISR(TIM0_COMPB_vect)
 {
-    TCCR0B |= BIT(FOC0A); //Force output compare A timer to implement PWM duty cycle
+    if(global_reg.pwm_enabled)
+        set_pin_low(FAN_PWM_PORT);
 }
 
 ISR(ADC_vect)
 {
-    global.prev_adc_value = global.curr_adc_value;
-    global.curr_adc_value = ADCH; //Ignore LSB of the result
+    global_ram.prev_adc_value = global_ram.curr_adc_value;
+    global_ram.curr_adc_value = ADCH; //Ignore LSB of the result
 }
 
 int main()
@@ -105,19 +111,19 @@ int main()
     do
     {
         sleep_cpu();
-    } while(!global.update_fan_speed);
+    } while(!global_reg.update_fan_speed);
         
 
     //Enforce speed percentage event change at the start
     cli();
-    global.prev_adc_value = 0;
+    global_ram.prev_adc_value = 0;
 
     //Enter program loop
     while(1)
     {
         cli();
-        uint8_t current = global.curr_adc_value;
-        uint8_t previous = global.prev_adc_value;
+        uint8_t current = global_ram.curr_adc_value;
+        uint8_t previous = global_ram.prev_adc_value;
         sei();
         if(current != previous)
         {
@@ -136,32 +142,29 @@ int main()
             }
             else
             {
-                if(!global.pwm_enabled)
+                if(!global_reg.pwm_enabled)
                 {
-                    set_pin_low(FAN_PWM_PORT);
-                    set_bit(TCCR0A, COM0A0); //Toggle PWM pin on time compare A event
-                    global.pwm_enabled = 1;
+                    global_reg.pwm_enabled = 1;
 
                     //Clear fan counters and timer counter to properly align toggling PWM pin
-                    global.fan_revolution_pulses = 0;
-                    global.fan_revolution_count = 0;
-                    TCNT0 = 0;
+                    global_ram.fan_revolution_pulses = 0;
+                    global_ram.fan_revolution_count = 0;
                 }
                 OCR0B = timer_b_val;
             }
 
-            global.display_delay = 0;
-            global.display_percentage = 1;
-            global.fan_revolution_pulses = 0;
+            global_reg.display_delay = 0;
+            global_reg.display_percentage = 1;
+            global_ram.fan_revolution_pulses = 0;
             sei();
 
             set_displayed_number(percent, AS_PERCENT);
         }
-        else if(!global.display_percentage && global.update_fan_speed)
+        else if(!global_reg.display_percentage && global_reg.update_fan_speed)
         {
             cli();
-            uint16_t revolutions = global.fan_revolution_count;
-            global.update_fan_speed = 0;
+            uint16_t revolutions = global_ram.fan_revolution_count;
+            global_reg.update_fan_speed = 0;
             sei();
             set_displayed_number(revolutions, AS_NUMBER);
         }
