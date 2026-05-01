63be77d048bdb8eea5d3eea864531bb39082819b
PookyFan <pookyfan@kancolle.pl>
2026-05-01 17:04:40 +0200

    Changed pins assignments

--- src/ports.h
+++ src/ports.h
@@ -5,9 +5,9 @@
 
 #define FAN_PWM_PORT   0
 #define FAN_SENSOR_PIN 1
-#define DISP_DATA_PORT 2
+#define DISP_DATA_PORT 4
 #define DISP_RCLK_PORT 3
-#define DISP_SCLK_PORT 4
+#define DISP_SCLK_PORT 2
 #define KNOB_ADC_PIN   5
 
 #define BIT(n) (1 << (n))
